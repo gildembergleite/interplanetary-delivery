ee120e61d9d82390590d94548720ff7afef97335
gildembergleite <gildembergleite@gmail.com>
2024-05-28 15:19:29 -0300

    feat: add navigate button in HomePage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,9 @@
+import { NavigateButton } from '@/components/client-components/navigate-button'
+
 export default function Home() {
-  return <main></main>
+  return (
+    <main className="flex w-full min-h-screen justify-center items-center gap-6">
+      <NavigateButton title="Addresses Page" page="/addresses" />
+    </main>
+  )
 }
